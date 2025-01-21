3314e7944756f5292e6bfa3009b595db5d545b07
GalinSavov <lemonkiller34@gmail.com>
2025-01-21 12:11:02 +0200

    Updated the Bootstrap theme of the application

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427ec51a314171b8e5ba431719099702aa141811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b432ddb13bc285893f852a0289739b9f2ee3609b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
