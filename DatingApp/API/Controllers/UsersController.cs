d0b26c54f5f39e7e15859e3b506e4650f8528cc6
GalinSavov <lemonkiller34@gmail.com>
2025-01-27 15:40:43 +0200

    Added an Automapper to the Users controller

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,27 +1,33 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
     {
         var users = await userRepository.GetAllUsersAsync();
-        return Ok(users);
+
+        var usersToReturn = mapper.Map<IEnumerable<MemberDTO>>(users);
+        return Ok(usersToReturn);
     }
 
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDTO>> GetUser(string username)
     {
         var user = await userRepository.GetUserByUsernameAsync(username);
 
-        if (user == null) return NotFound();
-        return user;
+        var usersToReturn = mapper.Map<MemberDTO>(user);
+
+        if (usersToReturn == null) return NotFound();
+        return usersToReturn;
     }
 }
--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b3a609712c3f021397a3b0ef793c5f2b614a95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ca287ccaeec16f9aa67a8c7cd84e2c4541b3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
