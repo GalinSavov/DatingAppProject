184d84a456b11a5ae35e22ce61c9ba2b3a9da53d
GalinSavov <lemonkiller34@gmail.com>
2025-01-28 09:40:10 +0200

    Added a Member interface

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0b26c54f5f39e7e15859e3b506e4650f8528cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c670a58e8b05c69f6211d867bf200b20ea1c44fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
