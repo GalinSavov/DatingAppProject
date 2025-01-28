0201a4d96e70b7cfa08d598054ba4d7aed8692b5
GalinSavov <lemonkiller34@gmail.com>
2025-01-28 12:02:09 +0200

    Added routing to member details page

--- DatingApp/client/src/app/members/member-details/member-details.component.ts
+++ DatingApp/client/src/app/members/member-details/member-details.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberService } from '../../_services/member.service';
+import { Member } from '../../_models/member';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-details',
   standalone: true,
   imports: [],
   templateUrl: './member-details.component.html',
-  styleUrl: './member-details.component.css'
+  styleUrl: './member-details.component.css',
 })
-export class MemberDetailsComponent {
+export class MemberDetailsComponent implements OnInit {
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.displayMember();
+  }
+
+  displayMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: (response) => (this.member = response),
+      error: (error) => console.log(error),
+    });
+  }
 }
