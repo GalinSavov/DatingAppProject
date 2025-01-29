d63472ab2bd7883ef9d1e02b2b11bf80f4c61c87
GalinSavov <lemonkiller34@gmail.com>
2025-01-29 16:08:40 +0200

    Added Cloudinary photo service to the API

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a111b9c3a5e296be526ce359d4b53b93a5a2199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc90fc7efdc168f1269874abacb4eeba94db75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
