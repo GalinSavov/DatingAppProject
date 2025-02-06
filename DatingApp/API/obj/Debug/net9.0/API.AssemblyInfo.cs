250e225b0e6a9043274aaa8df906dcf3f4dcf480
GalinSavov <lemonkiller34@gmail.com>
2025-02-06 10:45:22 +0200

    Added a reusable text input component

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8323cc0f74e8644d4819f3a79092813eaa9cacd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da0428b00f2d2fd6afd5c00487db080934ca14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
