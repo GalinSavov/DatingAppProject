7fc022884da2d1ae4c870e3db76cefcc3b9a8c7e
GalinSavov <lemonkiller34@gmail.com>
2025-02-13 10:31:18 +0200

    Added a Likes entity

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -1,8 +1,28 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public required DbSet<AppUser> Users { get; set; }
+    public required DbSet<UserLike> Likes { get; set; }
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserLike>().HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+        modelBuilder.Entity<UserLike>().
+        HasOne(s => s.SourceUser).
+        WithMany(l => l.LikedUsers).
+        HasForeignKey(k => k.SourceUserId).
+        OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserLike>().
+        HasOne(t => t.TargetUser).
+        WithMany(l => l.LikedByOtherUsers).
+        HasForeignKey(k => k.TargetUserId).
+        OnDelete(DeleteBehavior.Cascade);
+    }
 }
--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead84964f7e92c3a5558088824cb98f3180110c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+572afdd88680c048d756606bbb0b53ed91904464")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
