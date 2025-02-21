97631c9cbf84f3a2d7ecd553f1de21e3b406833e
GalinSavov <lemonkiller34@gmail.com>
2025-02-21 14:32:30 +0200

    Updated the token service

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279346563504cb52bd872932216ef389025565dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec0f025c99e9575632a6611bd22783a675d4159")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
