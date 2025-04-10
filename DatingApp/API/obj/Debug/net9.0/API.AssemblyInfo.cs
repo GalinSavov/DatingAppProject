987e96802e7faf1f10bd896eb66732db12884835
GalinSavov <lemonkiller34@gmail.com>
2025-04-10 11:02:05 +0300

    updated sql version

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d375fe7179de090264777c590144146833bdcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b867b4050cd92cfc36d81a42e7252320c8d2f0b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
