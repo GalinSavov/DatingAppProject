154217cb930a07df5ba7852052c96d6a9930ad3b
GalinSavov <lemonkiller34@gmail.com>
2025-04-10 11:58:47 +0300

    moved the migrations folder

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48604c1a355c509eb2b02bab778bf41cbcb0e217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f0721ebb38720cc9405ba9a8e611d6c68d4938")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
